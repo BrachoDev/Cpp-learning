0aface2e217e657ab5cdcd3f2c50e12eeed5d887
Carlos Bracho <brachocarlos152@gmail.com>
2024-10-26 14:26:14 -0400

    added folder for cs-210 lab activities. Also, put a initial code in practice for help

--- pratice.cpp
+++ pratice.cpp
@@ -1,27 +1,7 @@
+// Use this file for practicing only
 #include <iostream>
-#include <iomanip>               //For setprecision
-using namespace std;
 
-int main() {
-
-    // Initializing variable:
-    double carMilage;
-    double gasCost;
-
-    // Getting user's input:
-    std::cin >> carMilage;
-    std::cin >> gasCost;
-
-    // Changing floats to two decimals
-    cout << fixed << setprecision(2);
-
-    // Calculating the price for 20, 75 and 500 miles
-    double twentyMiles = (20 / carMilage) * gasCost;
-    double seventyFiveMiles = (75 / carMilage) * gasCost;
-    double fiveHundredMiles = (500 / carMilage) * gasCost;
-
-    // Showing results in the terminal
-    std::cout << twentyMiles << " " << seventyFiveMiles << " " << fiveHundredMiles << std::endl;
-
+int main(){
+    std::cout << "I love coding";
     return 0;
 }
