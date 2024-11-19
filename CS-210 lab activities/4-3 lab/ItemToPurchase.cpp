0cfde83a49fcba137b08301a6b69f5ab2582276c
Carlos Bracho <brachocarlos152@gmail.com>
2024-11-19 17:53:13 -0500

    ItemToPurchase.h: initialized itemName, itemPrice and itemQuantity.

---
+++ CS-210 lab activities/4-3 lab/ItemToPurchase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+// Contructor
+ItemToPurchase::ItemToPurchase() {
+    itemName = "none";
+    itemPrice = 0;
+    itemQuantity = 0;
+}
+
+// Setter
